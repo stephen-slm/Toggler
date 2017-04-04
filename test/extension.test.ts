e9faea63f8f888d3310d95caf07c2cae2a69cd7f
Stephen <smmstephenmiller@gmail.com>
2017-04-04 11:45:43 +0100

    fixed default tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,20 +1,18 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
-//loadin the Configuration
-const defaults = require('../src/defaults.json');
+// Settings the configuration default
+const defaults: any = [
+    ["0", "1"],
+    ["enable", "disable"],
+    ["enabled", "disabled"],
+    ["on", "off"],
+    ["true", "false"],
+    ["yes", "no"]
+]
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("VS-Toggler Tests", function() {
 
     test("Configuration should be a valid array of string words", () => {
